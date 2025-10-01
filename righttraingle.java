3f449fafd9cf0b34abefd76e386506fd51cdf13b
GAYATHIRI <gayathiribala15@gmail.com>
2025-10-01 19:12:42 +0530

    Add files via upload

---
+++ righttraingle.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class righttraingle {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            //space
+            for(int j=i;j<=n-1;j++){
+                System.out.print(" ");
+            }
+            for(int k=1;k<=i;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        }
+    }
+    
+
